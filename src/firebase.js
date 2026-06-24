cb2b7393c3deacddecde1a93adc56bf7e746ba4c
mvaquero01 <mvaquero010305@gmail.com>
2026-06-24 02:13:12 +0200

    vincular

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "peluqueria-vaquero-c3e53.firebaseapp.com",
-  projectId: "peluqueria-vaquero-c3e53",
-  storageBucket: "peluqueria-vaquero-c3e53.firebasestorage.app",
-  messagingSenderId: "316425507808",
-  appId: "1:316425507808:web:b42c5e68955e5904b69185"
+  authDomain: "marc-ros-barber-b17bd.firebaseapp.com",
+  projectId: "marc-ros-barber-b17bd",
+  storageBucket: "marc-ros-barber-b17bd.firebasestorage.app",
+  messagingSenderId: "402397544504",
+  appId: "1:402397544504:web:066170cfbbee53d3bfa6cd"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
